95a8bcd0cb18ac575578555749562a6f984c44f1
mcmak <maimcmak@outlook.com>
2022-11-09 12:16:08 +0800

    6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,20 @@
-
+import { useEffect } from 'react'
 import AnecodoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { setAnecdote } from './reducers/store'
+import { useDispatch } from 'react-redux'
+import anecdotesService from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdotesService
+      .getAll().then(anecdotes => dispatch(setAnecdote(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
 
 const filterSlice = createSlice({
   name: 'filter',
